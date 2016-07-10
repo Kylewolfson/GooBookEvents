f364dbc93bdd6aeb5c7c77b0eb4aa006b59a1385
Kyle Wolfson <wolfsonk@gmail.com>
2016-07-10 12:00:20 -0700

    Got a list view working with event name displaying. Forgot to commit when JSON data was originally returning properly. Part of the issue was that the data was 2 layers deep in the return object and part was just sorting out my code. That update is also included in this commit.

--- app/src/main/java/com/epicodus/facebook_googleeventsync/ui/EventSelect.java
+++ app/src/main/java/com/epicodus/facebook_googleeventsync/ui/EventSelect.java
@@ -3,6 +3,8 @@ package com.epicodus.facebook_googleeventsync.ui;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.epicodus.facebook_googleeventsync.R;
 import com.facebook.GraphRequest;
@@ -16,16 +18,23 @@ import org.json.JSONObject;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import models.Event;
 import okhttp3.Response;
 
 public class EventSelect extends AppCompatActivity {
     public static final String TAG = EventSelect.class.getSimpleName();
 
+    @Bind(R.id.listView) ListView mListView;
+
+    public ArrayList<Event> mEvents = new ArrayList();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_select);
+        ButterKnife.bind(this);
         getEvents("placeholder");
     }
 
@@ -38,6 +47,21 @@ public class EventSelect extends AppCompatActivity {
                         String jsonData = response.toString();
                         Log.v(TAG, jsonData);
                         mEvents = processResults(response);
+
+                        EventSelect.this.runOnUiThread(new Runnable() {
+
+                            @Override
+                            public void run() {
+                                String[] eventNames = new String[mEvents.size()];
+                                for (int i = 0; i < eventNames.length; i++) {
+                                    eventNames[i] = mEvents.get(i).getName();
+                                }
+
+                                ArrayAdapter adapter = new ArrayAdapter(EventSelect.this, android.R.layout.simple_list_item_1, eventNames);
+                                mListView.setAdapter(adapter);
+                            }
+
+                        });
                     }
                 });
 
@@ -46,36 +70,31 @@ public class EventSelect extends AppCompatActivity {
         request.setParameters(parameters);
         request.executeAsync();
         };
-    public ArrayList<Event> processResults(Response response) {
+
+    public ArrayList<Event> processResults(GraphResponse response) {
         ArrayList<Event> events = new ArrayList<>();
 
         try {
-            String jsonData = response.body().string();
-            if (response.isSuccessful()) {
-                JSONObject fbJSON = new JSONObject(jsonData);
-                JSONArray eventsJSON = fbJSON.getJSONArray("events");
+            JSONObject fbJSON  = response.getJSONObject();
+            JSONObject drilldownJSON = fbJSON.getJSONObject("events");
+            JSONArray eventsJSON = drilldownJSON.getJSONArray("data");
                 for (int i = 0; i < eventsJSON.length(); i++) {
                     JSONObject eventJSON = eventsJSON.getJSONObject(i);
 
                     String name = eventJSON.getString("name");
                     String rsvp = eventJSON.getString("rsvp_status");
-                    String endTime = eventJSON.getString("end_time");
+                    String endTime = eventJSON.optString("end_time", "No end time provided");
                     String startTime = eventJSON.getString("start_time");
                     String description = eventJSON.getString("description");
-                    String place = eventJSON.getJSONArray("place").get(0).toString();
+                    String place = eventJSON.getJSONObject("place").toString();
                     Event event = new Event(description, endTime, name, startTime, rsvp, place);
                     events.add(event);
                 }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
         } catch (JSONException e) {
             e.printStackTrace();
         }
         return events;
     }
-
-
 }
 
 
