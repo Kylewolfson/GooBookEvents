13947937d756bd9736612cb73762b1bf368e46c8
Kyle Wolfson <wolfsonk@gmail.com>
2016-07-15 14:28:31 -0700

    changed model to a parcelable for intent passing, changed Event to FacebookEvent because of naming conflict, added code to prevent duplication of events, did not commit often enough. Next step is to hook up incoming events with synch activity.

---
+++ app/src/main/java/models/FacebookEvent.java
@@ -0,0 +1,52 @@
+package models;
+
+import org.parceler.Parcel;
+
+/**
+ * Created by Kyle on 7/8/2016.
+ */
+
+@Parcel
+public class Event {
+     String description;
+     String endTime;
+     String name;
+     String startTime;
+     String rsvp;
+     String place;
+
+
+    public Event(String description, String endTime, String name, String startTime, String rsvp, String place) {
+        this.description = description;
+        this.endTime = endTime;
+        this.name = name;
+        this.startTime = startTime;
+        this.rsvp = rsvp;
+        this.place = place;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public String getRsvp() {
+        return rsvp;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+}
+
